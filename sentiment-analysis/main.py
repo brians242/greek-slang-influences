eca17ec2e5bf9149e69eb19b2e4bc90a95916a87
Karasmai <briansu242@gmail.com>
2022-11-12 19:46:08 -0800

    adding some starting folders, fixing some organization

---
+++ sentiment-analysis/main.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt  # modeling
+import bs4 as bs  # scraping for languages
+import numpy as np
+import pandas as pd 
+import nltk as ntlk
+
+import os  # for file management if needed
+
+""" This is our project.
+"""
+
+def main():
+    pass
+
+"""
+Main sentiment-analysis/ scraping starting template
+"""
+
+if __name__ == "__main__":
+    main()
