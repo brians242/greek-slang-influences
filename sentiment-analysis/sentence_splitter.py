b060f86ba970361a37258ead6027ebcce89b5d1d
Karasmai <briansu242@gmail.com>
2024-01-15 18:54:54 -0800

    making sure to place comparisons in, refactoring slightly

--- sentiment-analysis/sentence_splitter.py
+++ sentiment-analysis/sentence_splitter.py
@@ -10,12 +10,12 @@ def main(input_file):
             sentence += line.strip() + " "
             # check line for a period
             if re.search(r'(?<=\w)\.(?=\s|$)', sentence):
-                # no clue
+                # it works still i think
                 sentence1 = re.findall(r'(.*?\.)(?=\s|$)', sentence)
                 for i in sentence1:
                     # remove base 10
                     i = re.sub(r'\d+\.\d+|\d+', '', i)
-                    # remove "" and periods
+                    # remove whitespace and periods
                     i = i.strip().strip('.')
                     if not i:
                         continue
@@ -30,18 +30,15 @@ def main(input_file):
                 continue
         # remaining text is reconsidered
         if sentence:
-            # same as above
             sentence = re.sub(r'\d+\.\d+|\d+', '', sentence)
-            # same as above
             sentence = sentence.strip().strip('.')
             if sentence:
-                # add to count for file names
                 count += 1
                 filename = f"{input_file.split('.')[0]}_{count}.csv"
                 with open(filename, 'w', newline='') as out:
                     writer = csv.writer(out)
                     writer.writerow([sentence])
 
-# make sure the main function only runs in this program
 if __name__ == "__main__":
+    main("iliad.txt")
     main("book2.txt")
